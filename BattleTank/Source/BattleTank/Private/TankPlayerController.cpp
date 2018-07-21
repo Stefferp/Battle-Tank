1e2d04d3359cd141704e066ffd918d49ec353774
Steffan Desktop <steffanmitrovic@live.nl>
2018-07-21 14:21:08 +0200

    Override the beginplay on TankPlayerController and log out its Tank

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,19 @@
 #include "TankPlayerController.h"
 #include "GameFramework/Pawn.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("TankController on BeginPlay"));
+	MyTank = GetControlledTank();
+	if (!MyTank) {
+		UE_LOG(LogTemp, Warning, TEXT("I do not possess a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("My tank is %s"), *MyTank->GetName());
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
